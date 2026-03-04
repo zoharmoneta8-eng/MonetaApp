10da6046323569b88500ce95794389c7e8452f03
Zohar Moneta <developer@farmapp.local>
2026-03-04 22:22:03 +0200

    use warm background color for page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="he" dir="rtl">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-orange-50`}
       >
         <Header />
         {children}
